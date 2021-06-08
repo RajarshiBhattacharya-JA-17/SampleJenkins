a67f97b213239be678b8bf0c976c335ebac13676
RajarshiBhattacharya-JA-17 <rajarshib1998@gmail.com>
2021-06-08 10:14:22 +0530

    2nd project file

---
+++ com/capgemini/samplejenkins/SampleJenkins2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.capgemini.samplejenkins;
+
+/**
+ * @author Rajarshi
+ *
+ */
+public class SampleJenkins2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello...this is a 2nd project...");
+	}
+
+}
