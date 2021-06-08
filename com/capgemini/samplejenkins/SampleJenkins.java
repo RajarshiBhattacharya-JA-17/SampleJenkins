a08eb89ffa7ce0872c189c8b23302405e46920d2
RajarshiBhattacharya-JA-17 <rajarshib1998@gmail.com>
2021-06-08 09:29:08 +0530

    initial commit

---
+++ com/capgemini/samplejenkins/SampleJenkins.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.capgemini.samplejenkins;
+
+/**
+ * @author Rajarshi
+ *
+ */
+public class SampleJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello...This is a Sample Jenkins Project");
+	}
+}
